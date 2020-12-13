75b94b7cd2414e54c628db78af5e40184c95b9dc
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-13 17:10:24 +0100

    create a card for each item

--- src/components/todo/todoItem.tsx
+++ src/components/todo/todoItem.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 export const TodoItem = () => {
     return (
         <div>
-            
+            <input type="text" />
+            <button>Add</button>
         </div>
     );
 }
