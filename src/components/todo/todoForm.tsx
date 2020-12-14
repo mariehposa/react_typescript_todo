c6845e37fb4bc68d8b5ab6fda9a52e590458ba57
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-14 12:58:13 +0100

    handle inout

--- src/components/todo/todoForm.tsx
+++ src/components/todo/todoForm.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 export const TodoForm = () => {
     const [ newTodo, setNewTodo ] = useState('');
 
-    
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value)
+    }
     return (
         <div>
-            <input type="text" />
+            <input type="text" value={newTodo} onChange={handleInput} />
             <button type="submit" >Add todo</button>
         </div>
     );
