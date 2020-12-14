77b8b92ab7d2cf690d70c8de804b53bdef4b7b82
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-14 19:52:01 +0100

    add more styles

--- src/components/todo/todoCard.tsx
+++ src/components/todo/todoCard.tsx
@@ -9,9 +9,11 @@ interface TodoItemProp {
 
 export const TodoCard: React.FC<TodoItemProp> = ({todo, changeTodo, removeTodo}) => {
     return (
-        <div>
-            <input type="checkbox" checked={todo.isCompleted} onChange={() => changeTodo(todo)} />
-            <p style={{textDecoration: todo.isCompleted ? "line-through" : "none"}} onClick={() => changeTodo(todo)}>{todo.item}</p>
+        <div className="card">
+            <div>
+                <input type="checkbox" checked={todo.isCompleted} onChange={() => changeTodo(todo)} />
+                <p style={{textDecoration: todo.isCompleted ? "line-through" : "none"}} onClick={() => changeTodo(todo)}>{todo.item}</p>
+            </div>
             <p onClick={() => removeTodo(todo.item)}>x</p>
         </div>
     );
