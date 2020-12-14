3b59ce4caa25abc24f223a531b35a0dece1ae6b0
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-14 12:44:29 +0100

    add form to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TodoList } from './components/todo/todoList';
+import { TodoForm } from "./components/todo/todoForm";
 
 const App: React.FC = () => {
 
   return (
     <>
       <TodoList />
+      <TodoForm />
     </>
   );
 }
--- src/components/todo/todoForm.tsx
+++ src/components/todo/todoForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Todoform = () => {
+export const TodoForm = () => {
     return (
         <div>
             <input type="text" />
